0284a5c7f62bb3c681e58f7d1c974a6557f63469
reid <reidsherman@gmail.com>
2017-10-16 21:42:33 -0700

    Finish homework

--- W3D2/plays.rb
+++ W3D2/plays.rb
@@ -15,13 +15,49 @@ class Playwrite
   attr_accessor :name, :birth_year, :id
   def self.all
     data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
-    data.map { |datum| Playwright.new(datum) }
+    # data.map { |datum| Playwright.new(datum) }
   end
+
+  def self.find_by_name(pw)
+    pw = PlayDBConnection.instance.execute(<<-SQL, pw)
+      SELECT
+        *
+      FROM
+        playwrights
+      WHERE
+        name = ?;
+    SQL
+    Playwright.new(pw.first)
+  end
+
   def initialize(options)
     @id = options['id']
     @name = options['name']
     @birth_year = options['birth_year']
   end
+
+  def create
+    raise "#{self} is already in db!" if @id
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year)
+      INSERT INTO
+        playwrights(name, birth_year)
+      VALUES
+        (?, ?);
+    SQL
+  end
+
+  def update
+    raise "#{self} is not in db!" unless @id
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year)
+    UPDATE
+      playwrights
+    SET
+      name = ?, birth_year = ?
+    WHERE
+      id = ?;
+    SQL
+  end
+
 end
 
 class Play
