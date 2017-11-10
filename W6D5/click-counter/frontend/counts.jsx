00586f6fa12398f5fc159cc35aaa7e4969c86f0a
reid <reidsherman@gmail.com>
2017-11-09 22:22:27 -0800

    Create webpack bundle config file

---
+++ W6D5/click-counter/frontend/counts.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+// const Counts = ({ previousCounts }) => (
+//   <ul>
+//     {
+//       previousCounts.map((count, idx) => <li key={idx}>{count}</li>)
+//     }
+//   </ul>
+// );
+
+// For lifecycle method demo:
+
+class Counts extends React.Component {
+  componentWillMount() { debugger; }
+  componentDidMount() { debugger; }
+  componentWillReceiveProps(nextProps) { debugger; }
+  componentWillUpdate(nextProps, nextState) { debugger; }
+  componentDidUpdate(prevProps, prevState) { debugger; }
+  componentWillUnmount() { debugger; } //frontend routing or remove from dom (single page frontend)
+
+  render() {
+    return (
+      <ul>
+        {
+          this.props.previousCounts.map(
+  (count, idx) => <li key={idx}>{count}</li>)
+        }
+      </ul>
+    )
+  }
+}
+
+export default Counts;
